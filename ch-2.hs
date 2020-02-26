0edcfe139f94f22339d55db9599466429fc00155
Christian54090 <christian.sanders@live.com>
2020-02-26 13:55:41 -0800

    Types and Functions

--- ch-2.hs
+++ ch-2.hs
@@ -2,3 +2,9 @@ myDrop :: Int -> [a] -> [a]
 myDrop 0 xs     = xs
 myDrop _ []     = []
 myDrop n (x:xs) = myDrop (n-1) xs
+
+lastButOne :: [a] -> a
+lastButOne []       = error "empty list"
+lastButOne (_:[])   = error "list too short"
+lastButOne (x:_:[]) = x
+lastButOne (x:xs)   = lastButOne xs
